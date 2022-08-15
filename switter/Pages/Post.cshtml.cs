3c0a0d8e733789dd050c060119ab2227bbf03f46
Administrator <Administrator@WIN-SNBADM1VI4P>
2022-08-16 00:26:35 +0300

    Add project files.

---
+++ switter/Pages/Post.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace switter.Pages
+{
+    public class PostModel : PageModel
+    {
+        private readonly ILogger<PostModel> _logger;
+
+        public PostModel(ILogger<PostModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
